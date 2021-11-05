681c92d49602ea0a43fc012c241b45390c766f48
Juan David Nicholls Cardona <jdnichollsc@hotmail.com>
2021-11-05 18:17:50 -0500

    Update tests

--- contract/assembly/__tests__/main.spec.ts
+++ contract/assembly/__tests__/main.spec.ts
@@ -3,12 +3,26 @@ import { BuyMeNear } from '..'
 describe('BuyMeNear', () => {
   
   it('should donations be zero when contract is created', () => {
-    const buymenear = new BuyMeNear()
+    const buymenear = new BuyMeNear();
     expect(buymenear.getTotalDonations()).toBe(0, "should be 0");
   });
 
   it('should accounts be empty list', () => {
-    const buymenear = new BuyMeNear()
+    const buymenear = new BuyMeNear();
     expect(buymenear.getAllUserAddresses().length).toBe(0, "should be 0");
+  });
+
+  it('should have an account when a profile is update', () => {
+    const buymenear = new BuyMeNear();
+    expect(buymenear.getAllUserAddresses().length).toBe(0, "should be 0");
+
+    buymenear.updateUserProfile(
+      'David',
+      'Gonzalez',
+      'Near Developer',
+      `I'm a experienced Developer looking for job opportunities with Blockchain and Smart contracts`,
+      'https://avatars.githubusercontent.com/u/1445496?v=4'
+    );
+    expect(buymenear.getAllUserAddresses().length).toBe(1, "should be 1");
   });
 });
