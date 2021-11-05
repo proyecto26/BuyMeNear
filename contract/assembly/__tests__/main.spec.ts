ae5c1b83cb01d3dff6bd4b78a9cc3bfc772c7be8
Juan David Nicholls Cardona <jdnichollsc@hotmail.com>
2021-11-05 18:56:38 -0500

    Fix unit tests

--- contract/assembly/__tests__/main.spec.ts
+++ contract/assembly/__tests__/main.spec.ts
@@ -1,20 +1,20 @@
 import { BuyMeNear } from '..'
 
 describe('BuyMeNear', () => {
-  
-  it('should donations be zero when contract is created', () => {
+
+  it('should accounts be an empty list', () => {
+    const buymenear = new BuyMeNear();
+    expect(buymenear.getUserAccounts().length).toBe(0, "should be 0");
+  });
+
+  it('should total donations be zero when contract is created', () => {
     const buymenear = new BuyMeNear();
     expect(buymenear.getTotalDonations()).toBe(0, "should be 0");
   });
 
-  it('should accounts be empty list', () => {
+  it('should have an account when a profile is updated', () => {
     const buymenear = new BuyMeNear();
-    expect(buymenear.getAllUserAddresses().length).toBe(0, "should be 0");
-  });
-
-  it('should have an account when a profile is update', () => {
-    const buymenear = new BuyMeNear();
-    expect(buymenear.getAllUserAddresses().length).toBe(0, "should be 0");
+    expect(buymenear.getUserAccounts().length).toBe(0, "should be 0");
 
     buymenear.updateUserProfile(
       'David',
@@ -23,6 +23,6 @@ describe('BuyMeNear', () => {
       `I'm a experienced Developer looking for job opportunities with Blockchain and Smart contracts`,
       'https://avatars.githubusercontent.com/u/1445496?v=4'
     );
-    expect(buymenear.getAllUserAddresses().length).toBe(1, "should be 1");
+    expect(buymenear.getUserAccounts().length).toBe(1, "should be 1");
   });
 });
