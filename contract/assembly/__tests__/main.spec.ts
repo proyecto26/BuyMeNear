1e95e83ee24b76f8d6555e89f94edccc3b52d8f1
Juan David Nicholls Cardona <jdnichollsc@hotmail.com>
2021-11-05 18:12:48 -0500

    Update tests

--- contract/assembly/__tests__/main.spec.ts
+++ contract/assembly/__tests__/main.spec.ts
@@ -1,27 +1,14 @@
 import { BuyMeNear } from '..'
-import { storage, Context, logging } from 'near-sdk-as'
 
-describe('Obtener el total de donaciones', () => {
-  it('Deberia estar en cero', () => {
+describe('BuyMeNear', () => {
+  
+  it('should donations be zero when contract is created', () => {
     const buymenear = new BuyMeNear()
-    expect(buymenear.getTotalDonations()).toBe(0, "Tiene que ser cero");
+    expect(buymenear.getTotalDonations()).toBe(0, "should be 0");
   });
 
-});
-
-describe('Probando cuentas', () => {
-  /*it('No deberian haber cuentas', () => {
+  it('should accounts be empty list', () => {
     const buymenear = new BuyMeNear()
-    var arr: string[] = [];
-    expect(buymenear.getAllUserAddresses()).toBe(arr, "No hay cuentas en este momento");
-  });*/
-
-  it('No deberian haber cuentas', () => {
-    const buymenear = new BuyMeNear()
-    var arr: string[] = [];
-    var user = buymenear.getUserProfile('managua.near');
-    console.log("" + user?.balance);
-    expect(user).actual;
+    expect(buymenear.getAllUserAddresses().length).toBe(0, "should be 0");
   });
-
 });
